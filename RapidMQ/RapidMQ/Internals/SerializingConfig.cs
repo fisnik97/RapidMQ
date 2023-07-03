c798390ffe185fa726303d874dd1f3322bafb34f
fisnik97 <ffisnikmaloku@hotmail.com>
2023-07-03 23:30:52 +0200

    Added JSON serializing options to publish & deserialze client messages. Added unbind queue from exchange method for clients too.

---
+++ RapidMQ/RapidMQ/Internals/SerializingConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace RapidMQ.Internals;
+
+internal static class SerializingConfig
+{
+    internal static JsonSerializerOptions DefaultOptions =>
+        new()
+        {
+            PropertyNameCaseInsensitive = true,
+            DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull,
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+        };
+}
