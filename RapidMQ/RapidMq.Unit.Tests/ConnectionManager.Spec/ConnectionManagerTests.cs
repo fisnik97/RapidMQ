9e9d4ffcd3333fbdc6bdc5fb6e9a4c4d068dbaae
fisnik97 <ffisnikmaloku@hotmail.com>
2023-09-16 22:46:37 +0200

    Added connection manager few tests and fixed the client's handler execution of tasks

--- RapidMQ/RapidMq.Unit.Tests/ConnectionManager.Spec/ConnectionManagerTests.cs
+++ RapidMQ/RapidMq.Unit.Tests/ConnectionManager.Spec/ConnectionManagerTests.cs
@@ -1,6 +1,51 @@
-﻿namespace RapidMq.Unit.Tests.ConnectionManager.Spec;
+﻿using Microsoft.Extensions.Logging;
+using Moq;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
+using RapidMQ.Models;
+using Xunit;
+
+namespace RapidMq.Unit.Tests.ConnectionManager.Spec;
 
 public class ConnectionManagerTests
 {
-    
+    private readonly Mock<ILogger<RapidMQ.ConnectionManager>> _mockLogger;
+    private readonly TestableConnectionManager _testableConnectionManager;
+    private readonly Mock<IConnection> _mockConnection;
+
+    private const string RabbitMqUri = "amqp://localhost";
+
+    public ConnectionManagerTests()
+    {
+        _mockLogger = new Mock<ILogger<RapidMQ.ConnectionManager>>();
+        _mockConnection = new Mock<IConnection>();
+        _testableConnectionManager = new TestableConnectionManager(_mockLogger.Object);
+    }
+
+    [Fact]
+    public async Task ConnectionManager_Should_Return_ConnectionInstance()
+    {
+        var config = new ConnectionManagerConfig(3, TimeSpan.FromMilliseconds(100), false);
+
+        var mockConnection = new Mock<IConnection>();
+        _testableConnectionManager.CreateConnectionInstanceMock = uri => mockConnection.Object;
+
+        var connection = await _testableConnectionManager.ConnectAsync(new Uri(RabbitMqUri), config);
+
+        Assert.NotNull(connection);
+        Assert.Same(mockConnection.Object, connection);
+    }
+
+
+    [Fact]
+    public async Task ConnectionManager_Should_Retry_Until_Successful()
+    {
+        var config = new ConnectionManagerConfig(3, TimeSpan.FromMilliseconds(100), false);
+
+        _testableConnectionManager.FailuresToSimulate = 2;
+
+        var connection = await _testableConnectionManager.ConnectAsync(new Uri(RabbitMqUri), config);
+
+        Assert.NotNull(connection);
+    }
 }
--- RapidMQ/RapidMq.Unit.Tests/ConnectionManager.Spec/TestableConnectionManager.cs
+++ RapidMQ/RapidMq.Unit.Tests/ConnectionManager.Spec/TestableConnectionManager.cs
@@ -1,6 +1,23 @@
-﻿namespace RapidMq.Unit.Tests.ConnectionManager.Spec;
+﻿using Microsoft.Extensions.Logging;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 
-public class TestableConnectionManager
+public class TestableConnectionManager : RapidMQ.ConnectionManager
 {
-    
+    public int FailuresToSimulate { get; set; } = 0;
+    private int _currentFailures = 0;
+    public Func<Uri, IConnection> CreateConnectionInstanceMock { get; set; }
+
+    public TestableConnectionManager(ILogger<RapidMQ.ConnectionManager> logger)
+        : base(logger)
+    {
+    }
+
+    protected override IConnection CreateConnectionInstance(Uri uri)
+    {
+        if (_currentFailures >= FailuresToSimulate)
+            return CreateConnectionInstanceMock?.Invoke(uri) ?? base.CreateConnectionInstance(uri);
+        _currentFailures++;
+        throw new BrokerUnreachableException(new Exception("Simulated connection failure."));
+    }
 }
